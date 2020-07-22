b5c5927e09583ff8feb3ca56db9d7c16b68bfc46
Usama Subhani <reachusama@protonmail.com>
2020-07-22 18:11:37 +0500

    Add test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { render, RenderResult, fireEvent, waitForDomChange } from '@testing-library/react';
 import App from './App';
 import NewTransaction from './components/NewTransaction';
 import ReactDOM from 'react-dom';
+import TransactionsList from './components/TransactionsList';
+import Transaction from './components/Transaction';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import { GlobalContext } from './context/GlobalContext';
 
 
 describe('<App />', () => {
@@ -13,19 +18,60 @@ describe('<App />', () => {
   });
 });
 
-describe('<NewTransaction />', () => {
-  let documentBody: RenderResult;
-  beforeEach(() => {
-    documentBody = render(<NewTransaction />);
-  });  
-  
-  it('Shows empty form with placeholders', () => {
-    expect(documentBody.getByPlaceholderText('Title')).toBeInTheDocument();
-    expect(documentBody.getByPlaceholderText('Amount')).toBeInTheDocument();
-  });
+test('Shows transaction title and amount', async () => {
+  const {getByTestId, getByText, debug} = render(
+    <Transaction id={1} title={"Test Title"} amount={666} />
+  )
+
+  expect(getByText("Test Title")).toBeInTheDocument();
+  expect(getByText(/666/)).toBeInTheDocument();
 });
-// test('Display empty form', () => {
-//   const { getByText } = render(<NewTransaction />);
-//   const linkElement = getByText(/Hello/);
-//   expect(linkElement).toBeInTheDocument();
-// });
+
+test('Shows Balance', async () => {
+  const {getByText} = render(
+    <Balance />
+  )
+
+  expect(getByText(/Balance/)).toBeInTheDocument();
+  expect(getByText(/\$/)).toBeInTheDocument();
+});
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<IncomeExpenses />, div);
+});
+
+test('Shows Income', () => {
+  const {getByText, debug} = render(
+    <IncomeExpenses />
+  )
+
+  expect(getByText(/Income/)).toBeInTheDocument();
+
+  // debug();
+});
+
+test('Shows Expense', () => {
+  const {getByText, debug} = render(
+    <IncomeExpenses />
+  )
+
+  expect(getByText(/Expense/)).toBeInTheDocument();
+
+  // debug();
+});
+
+test('Context', () => {
+  const {getByText, debug} = render(
+    <GlobalContext.Provider value={{
+      transactions: [{id:1, title:"Test", amount:66}],
+      deleteTransaction : () => {},
+      addTransaction :  () => {}
+    }}>
+      <App />
+    </GlobalContext.Provider>
+  )
+
+  expect(getByText)
+  debug();
+})
